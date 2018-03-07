87a8cfd91e1e83e37217b095d719881ca6798dd3
shiveen.pandita <shiveenpandita@outlook.com>
2018-03-07 22:38:17 +1100

    added new frontend vue site and basic templates and a bit of aboutme section

---
+++ frontend/src/components/aboutme.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="aboutme">
+    <h1>shiveen pandita</h1>
+    <p>Hi, I'm Shiveen. I'm a developer and generally consider myself as a lifelong learner. I created this website as
+      my own little personal space on the web and usually spend time writing about things that interest me.
+    </p>
+    <h5>Professional Experience</h5>
+    <ul>
+      <li>
+        Experience Java developer with over 4 years of experience in Spring and related technologies
+      </li>
+      <li>
+        Almost 2 years of experience in Angular (starting from AngularJS)
+      </li>
+      <li>
+        Proficient in Python, VueJS and a bit of bash glue scripting
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "aboutme"
+  }
+</script>
+
+<style scoped>
+  #aboutme {
+    margin: auto;
+    width: 50%;
+    padding: 10px;
+  }
+
+</style>
---
+++ frontend/src/components/navbar.vue
@@ -0,0 +1,27 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="#">SP</a>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item active">
+          <a class="nav-link" href="#">About Me<span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Blog</a>
+        </li>
+      </ul>
+    </div>
+  </nav>
+</template>
+
+<script>
+  export default {
+    name: "navbar"
+  }
+</script>
+
+<style scoped>
+  nav {
+    margin-bottom: 2em;
+  }
+</style>
